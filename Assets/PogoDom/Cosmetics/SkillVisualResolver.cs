359b8381b41e52318e8d2801889c34b5071465f3
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-06 09:15:05 -0300

    refactor: centralize skill event mapping

--- Assets/PogoDom/Cosmetics/SkillVisualResolver.cs
+++ Assets/PogoDom/Cosmetics/SkillVisualResolver.cs
@@ -6,7 +6,7 @@ namespace PogoDom.Cosmetics
     {
         public static SkillVisualDefinition Resolve(CosmeticCatalog catalog, CosmeticLoadout loadout, MatchEvent e)
         {
-            var trigger = FromEvent(e);
+            var trigger = VisualEventPolicy.TriggerFor(e);
             if (!trigger.HasValue) return null;
 
             SkillVisualDefinition generic = null;
@@ -24,25 +24,6 @@ namespace PogoDom.Cosmetics
                     generic = visual;
             }
             return generic;
-        }
-
-        private static SkillVisualTrigger? FromEvent(MatchEvent e)
-        {
-            switch (e.Type)
-            {
-                case MatchEventType.TileStolen: return SkillVisualTrigger.TileSteal;
-                case MatchEventType.TileProtected: return SkillVisualTrigger.ShieldBlock;
-                case MatchEventType.EnclosureCaptured: return SkillVisualTrigger.AreaCapture;
-                case MatchEventType.Banked: return SkillVisualTrigger.Bank;
-                case MatchEventType.CrateOpened: return SkillVisualTrigger.CrateOpen;
-                case MatchEventType.ArrowUsed: return SkillVisualTrigger.Arrow;
-                case MatchEventType.SpeedActivated: return SkillVisualTrigger.Speed;
-                case MatchEventType.MissileFired: return SkillVisualTrigger.Missile;
-                case MatchEventType.PadlockActivated: return SkillVisualTrigger.Padlock;
-                case MatchEventType.PlayerStunned: return SkillVisualTrigger.Stun;
-                case MatchEventType.HazardDetonated: return SkillVisualTrigger.HazardBlast;
-                default: return null;
-            }
         }
     }
 }

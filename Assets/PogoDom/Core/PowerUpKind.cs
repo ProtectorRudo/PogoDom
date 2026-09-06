8788f33005390b70a5aebc38230bdc24f97cc344
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-06 00:01:21 -0300

    bootstrap: add power-up enum

---
+++ Assets/PogoDom/Core/PowerUpKind.cs
@@ -0,0 +1,15 @@
+namespace PogoDom.Core
+{
+    public enum PowerUpKind
+    {
+        None = 0,
+        BankCrate = 1,
+        Arrow = 2,
+        Speed = 3,
+        Missile = 4,
+        FourWayMissile = 5,
+        ElectroStun = 6,
+        Padlock = 7,
+        Tnt = 8
+    }
+}

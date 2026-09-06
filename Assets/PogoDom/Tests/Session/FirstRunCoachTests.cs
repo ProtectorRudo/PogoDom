037865b63f963a1bbc053ca276a1d8fe86bcf9bd
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-06 10:11:36 -0300

    test: certify non-blocking runtime coach event feed

--- Assets/PogoDom/Tests/Session/FirstRunCoachTests.cs
+++ Assets/PogoDom/Tests/Session/FirstRunCoachTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using PogoDom.Core;
 using PogoDom.Session;
@@ -18,6 +19,7 @@ namespace PogoDom.Tests.Session
             Assert.IsTrue(coach.IsComplete);
             Assert.AreEqual(CoachHint.None, coach.Current.Hint);
             Assert.IsFalse(coach.Current.IsBlocking);
+            Assert.AreEqual(20f, coach.ElapsedSeconds);
         }
 
         [Test]
@@ -38,6 +40,38 @@ namespace PogoDom.Tests.Session
             bank.Events.Add(new MatchEvent(MatchEventType.Banked, 0, value: 5));
             coach.Observe(bank, 0.5f);
             Assert.IsTrue(coach.IsComplete);
+        }
+
+        [Test]
+        public void PresentationEventFeedCanDriveCoachWithoutTickResult()
+        {
+            var coach = new FirstRunCoach(0);
+            coach.ObserveEvents(new List<MatchEvent>
+            {
+                new MatchEvent(MatchEventType.PlayerMoved, 0),
+                new MatchEvent(MatchEventType.TilePainted, 0)
+            }, 0f);
+
+            Assert.AreEqual(CoachHint.BankToScore, coach.Current.Hint);
+            Assert.AreEqual(0f, coach.ElapsedSeconds);
+
+            coach.AdvanceWithoutTick(1.25f);
+            Assert.AreEqual(1.25f, coach.ElapsedSeconds);
+        }
+
+        [Test]
+        public void BotEventsCannotAdvanceLocalCoach()
+        {
+            var coach = new FirstRunCoach(0);
+            coach.ObserveEvents(new List<MatchEvent>
+            {
+                new MatchEvent(MatchEventType.PlayerMoved, 2),
+                new MatchEvent(MatchEventType.TileStolen, 2),
+                new MatchEvent(MatchEventType.Banked, 2, value: 9)
+            }, 0.5f);
+
+            Assert.AreEqual(CoachHint.SwipeToTurn, coach.Current.Hint);
+            Assert.IsFalse(coach.IsComplete);
         }
     }
 }
